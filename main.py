77ff51064045c41019d1de971b9089db59f5cb43
rbhavesh1525 <129406732+rbhavesh1525@users.noreply.github.com>
2025-10-12 08:57:37 +0530

    Update .gitignore and backend changes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 # CORRECTED: Import supabase_anon, not supabase
 from DbConfig.db import supabase_anon 
 
+from fastapi.middleware.cors import CORSMiddleware
 # Import routers
 from routes.mlresponse_route import router as ml_router
 # CORRECTED: Import the new auth router
@@ -16,6 +17,20 @@ app.include_router(history_routes.router, prefix="/api", tags=["History"])
 app.include_router(ml_router, prefix="/api", tags=["ML Response"])
 # CORRECTED: Include the auth router to make /signup and /login work
 app.include_router(authRoutes.router, prefix="/api/auth", tags=["Authentication"])
+
+origins = [
+    "http://localhost:5173",  # your frontend URL
+    "http://127.0.0.1:8000",  # in case Vite uses 127.0.0.1
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # or ["*"] for all origins (not recommended in prod)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # Test Supabase
